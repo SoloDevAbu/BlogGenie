987cccde6a023b56f007b4ed58cd7843703c4a50
SoloDevAbu <abubakkar2502@gmail.com>
2025-01-03 23:18:57 +0530

    First try

--- frontend/src/components/Home/Card.jsx
+++ frontend/src/components/Home/Card.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import panda from '../../assets/panda.webp'
 import arrow from '../../assets/right-arrow.png'
 
-const Card = ({img, heading, summary}) => {
+const Card = ({image, title, summary}) => {
   return (
     <div className=' bg-neutral-300 p-2 md:p-4 rounded-md'>
-        <img src={panda} alt="Source Image" className='rounded-md'/>
+        <img src={image} alt={title} className='rounded-md'/>
         <div className='pt-2'>
-            <h1 className='font-bold font-sans text-sm'>Here the heading will be present</h1>
-            <p className='text-xs py-1'>This section is all about the summary and just a random thing for design so i can write whatever i want just to fill this area</p>
+            <h1 className='font-bold font-sans text-sm'>{title}</h1>
+            <p className='text-xs py-1'>{summary}</p>
         </div>
         <div className='flex bg-neutral-800 justify-center items-center gap-2 rounded-md mt-2'>
             <button className='text-white'>Read Full Article</button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,7 @@ const Home = () => {
     <div className='flex flex-col'>
         <Hero/>
         <div className='grid grid-cols-2 justify-between gap-4 px-5 md:grid-cols-3 lg:grid-cols-4 pt-5 md:pt-8 lg:pt-10'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            
         </div>
     </div>
   )
