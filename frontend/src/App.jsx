4632cb428b97418d77e8e8c9eae31824a6f74e3c
SoloDevAbu <abubakkar2502@gmail.com>
2025-01-04 14:58:27 +0530

    Wrap the Home component inside browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
+import React, { Suspense, lazy } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+const Home = lazy(() => import('./pages/Home'))
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
